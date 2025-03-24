c9b324efafe9981774d8e3bcaa3b865f761eb771
Naman Jain <naman.jain@salesforce.com>
2025-03-24 18:05:32 +0530

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 async function getStarredRepos(githubUsername) {
-    const response = await fetch(`https://api.github.com/users/${githubUsername}/repos`);
+    const response = await fetch(
+        `https://api.github.com/users/${githubUsername}/repos`
+    );
+    console.log(response);
     const result = await response.json();
     console.log(result);
-    return result.filter(repo => repo.stargazers_count > 0)
+    return result.filter((repo) => repo.stargazers_count > 0);
 }
 
-(async function() {
-    const GITHUB_USERNAME = "naman09"
-    console.log(await getStarredRepos(GITHUB_USERNAME));
+function getProjectCardHTML() {}
+async function fillRepoDetails(repo) {
+    document.getElementById("project-name").innerHTML = repo.name;
+
+    document.getElementById("project-description").innerHTML =
+        await getProjectDescription(repo);
+
+    document.getElementById("project-last-updated-date").innerHTML = new Date(
+        repo.updated_at
+    ).toDateString();
+
+    document.getElementById("project-url").href = repo.html_url;
+}
+async function getProjectDescription(repo) {
+    if (repo.description) {
+        return repo.description;
+    }
+    const response = await fetch(
+        `https://raw.githubusercontent.com/${repo.owner.login}/${repo.name}/main/README.md`
+    );
+    console.log(response);
+    const mdText = await response.text();
+    const normalText = mdText.replace(/[^a-zA-Z ]/g, " ");
+    return (
+        normalText.slice(0, 100) +
+        (normalText.length > 100
+            ? `<a href="${repo.html_url}/blob/main/README.md" target="_blank">...</a>`
+            : "")
+    );
+}
+
+(async function () {
+    const GITHUB_USERNAME = "naman09";
+    const repos = await getStarredRepos(GITHUB_USERNAME);
+    console.log(repos);
+    await fillRepoDetails(repos[0]);
 })();
